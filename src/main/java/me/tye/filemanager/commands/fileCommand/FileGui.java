9765124a92adf9aff1dfeca8c3c237bd80acbda2
Mapty231 <tye.exe@gmail.com>
2023-09-04 00:51:32 +0100

    Added support for editing files

--- src/main/java/me/tye/filemanager/commands/fileCommand/FileGui.java
+++ src/main/java/me/tye/filemanager/commands/fileCommand/FileGui.java
@@ -2,6 +2,7 @@ package me.tye.filemanager.commands.fileCommand;
 
 import me.tye.filemanager.FileManager;
 import me.tye.filemanager.util.PathHolder;
+import net.wesjd.anvilgui.AnvilGUI;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -10,10 +11,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.BookMeta;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -22,6 +21,7 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static me.tye.filemanager.FileManager.itemProperties;
@@ -30,7 +30,7 @@ import static org.bukkit.plugin.java.JavaPlugin.getPlugin;
 
 public class FileGui implements Listener {
     public static void openFolder(Player player) {
-        Inventory gui = Bukkit.createInventory(player, 54, ChatColor.BLUE+position.get(player.getName()).getRelativePath()+ChatColor.GOLD+" $");
+        Inventory gui = Bukkit.createInventory(player, 54, ChatColor.BLUE+"~"+position.get(player.getName()).getRelativePath()+ChatColor.GOLD+" $");
         List<Path> paths;
         try {
             paths = Files.list(Path.of(position.get(player.getName()).getCurrentPath())).toList();
@@ -72,7 +72,7 @@ public class FileGui implements Listener {
     }
 
     public static void openFile(Player player, int lineNumber) {
-        Inventory gui = Bukkit.createInventory(player, 54, ChatColor.BLUE+position.get(player.getName()).getRelativePath().substring(0, position.get(player.getName()).getRelativePath().length()-1)+ChatColor.GOLD+" $");
+        Inventory gui = Bukkit.createInventory(player, 54, ChatColor.BLUE+"~"+position.get(player.getName()).getRelativePath().substring(0, position.get(player.getName()).getRelativePath().length()-1)+ChatColor.GOLD+" $");
 
         File file = new File(position.get(player.getName()).getCurrentPath());
         if (!file.isFile()) return;
@@ -91,16 +91,20 @@ public class FileGui implements Listener {
             return;
         }
 
+        if (lineNumber > lines.size()) lineNumber = lines.size();
+        if (lineNumber < 0) lineNumber = 1;
+
         ArrayList<ItemStack> content = new ArrayList<>();
         for (int i = 0; i <= 8; i++) {
             if (i == 0) {
-                content.add(itemProperties(new ItemStack(Material.ARROW), "Back", List.of("Exits the file.")));
+                content.add(itemProperties(new ItemStack(Material.ARROW), "Back", List.of("Exit the file.")));
             }
             else if (i == 2) {
-                content.add(itemProperties(new ItemStack(Material.OAK_SIGN), "Line number: "+lineNumber, List.of("The line number of the first line.")));
+                content.add(itemProperties(new ItemStack(Material.OAK_SIGN), "Line number: "+lineNumber, List.of("The line number of the first visible line.")));
             }
             else if (i == 3) {
-                content.add(itemProperties(new ItemStack(Material.WRITABLE_BOOK), "Search (WIP)", List.of("Finds instances of certain words.")));
+                //content.add(itemProperties(new ItemStack(Material.WRITABLE_BOOK), "Search (WIP)", List.of("Finds instances of certain words.")));
+                content.add(itemProperties(new ItemStack(Material.GRAY_STAINED_GLASS_PANE), "", null));
             }
             else if (i == 4) {
                 content.add(itemProperties(new ItemStack(Material.BARRIER), "Exit", List.of("Closes the gui.")));
@@ -108,8 +112,9 @@ public class FileGui implements Listener {
             else if (i == 5) {
                 ItemStack goTo = itemProperties(new ItemStack(Material.SPECTRAL_ARROW), "Go to", List.of("Go to a certain line by number."));
                 ItemMeta goToMeta = goTo.getItemMeta();
-                goToMeta.getPersistentDataContainer().set(new NamespacedKey(getPlugin(FileManager.class), "max"), PersistentDataType.INTEGER, lines.size()-4);
+                goToMeta.getPersistentDataContainer().set(new NamespacedKey(getPlugin(FileManager.class), "max"), PersistentDataType.INTEGER, lines.size());
                 goToMeta.getPersistentDataContainer().set(new NamespacedKey(getPlugin(FileManager.class), "currentLine"), PersistentDataType.INTEGER, lineNumber);
+                goToMeta.getPersistentDataContainer().set(new NamespacedKey(JavaPlugin.getPlugin(FileManager.class), "type"), PersistentDataType.STRING, "goto");
                 goTo.setItemMeta(goToMeta);
                 content.add(goTo);
             }
@@ -128,7 +133,7 @@ public class FileGui implements Listener {
                 content.add(down);
             }
             else {
-                content.add(new ItemStack(Material.GRAY_STAINED_GLASS_PANE));
+                content.add(itemProperties(new ItemStack(Material.GRAY_STAINED_GLASS_PANE), "", null));
             }
         }
 
@@ -142,8 +147,10 @@ public class FileGui implements Listener {
                     if (!paperName.equals("")) {
                         ItemStack paper = itemProperties(new ItemStack(Material.PAPER), paperName, null);
                         ItemMeta paperMeta = paper.getItemMeta();
-                        paperMeta.getPersistentDataContainer().set(new NamespacedKey(getPlugin(FileManager.class), "line"), PersistentDataType.INTEGER, i+lineNumber-1);
+                        paperMeta.getPersistentDataContainer().set(new NamespacedKey(getPlugin(FileManager.class), "line"), PersistentDataType.INTEGER, i+lineNumber);
                         paperMeta.getPersistentDataContainer().set(new NamespacedKey(getPlugin(FileManager.class), "offset"), PersistentDataType.INTEGER, ii);
+                        paperMeta.getPersistentDataContainer().set(new NamespacedKey(getPlugin(FileManager.class), "edited"), PersistentDataType.BOOLEAN, false);
+                        paperMeta.getPersistentDataContainer().set(new NamespacedKey(JavaPlugin.getPlugin(FileManager.class), "type"), PersistentDataType.STRING, "text");
                         paper.setItemMeta(paperMeta);
                         content.add(paper);
                     }
@@ -158,6 +165,37 @@ public class FileGui implements Listener {
 
         gui.setContents(content.toArray(new ItemStack[0]));
         player.openInventory(gui);
+    }
+
+    public static void editFile(Player player, ItemStack editedText) {
+        if (editedText.getItemMeta() == null) return;
+        PathHolder pathHolder = position.get(player.getName());
+        Integer line = editedText.getItemMeta().getPersistentDataContainer().get(new NamespacedKey(JavaPlugin.getPlugin(FileManager.class), "line"), PersistentDataType.INTEGER);
+        Integer offset = editedText.getItemMeta().getPersistentDataContainer().get(new NamespacedKey(JavaPlugin.getPlugin(FileManager.class), "offset"), PersistentDataType.INTEGER);
+        if (line == null|| offset == null) return;
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(pathHolder.getCurrentPath()));
+            StringBuilder content = new StringBuilder();
+
+            String text;
+            int i = 1;
+            while ((text = br.readLine()) != null) {
+                if (line == i) {
+                    int startOfReplace = 35*offset;
+                    String start = text.substring(0, startOfReplace);
+                    String end = text.substring(Math.min(startOfReplace+35, text.length()));
+                    text = start+editedText.getItemMeta().getDisplayName()+end;
+                }
+                i++;
+                content.append(text).append("\n");
+            };
+            br.close();
+            Files.writeString(Path.of(pathHolder.getCurrentPath()), content.toString());
+        } catch (Exception ex) {
+            player.sendMessage(ChatColor.RED+"There was an error trying to edit that file.\nPlease see the console for error message and report this.");
+            ex.printStackTrace();
+        }
     }
 
     @EventHandler
@@ -222,14 +260,40 @@ public class FileGui implements Listener {
     public void fileGoTo(InventoryClickEvent e) {
         if (e.getWhoClicked() instanceof Player player) {
             if (e.getCurrentItem() == null || e.getCurrentItem().getItemMeta() == null || e.getCurrentItem().getType() != Material.SPECTRAL_ARROW || !position.containsKey(player.getName())) return;
+            if (checkType(e.getCurrentItem(), "goto")) return;
             int max = e.getCurrentItem().getItemMeta().getPersistentDataContainer().get(new NamespacedKey(JavaPlugin.getPlugin(FileManager.class), "max"), PersistentDataType.INTEGER);
-            int currentLine = e.getCurrentItem().getItemMeta().getPersistentDataContainer().get(new NamespacedKey(JavaPlugin.getPlugin(FileManager.class), "max"), PersistentDataType.INTEGER);
-            ItemStack book = itemProperties(new ItemStack(Material.WRITTEN_BOOK), null, null);
-            BookMeta bookMeta = (BookMeta) book.getItemMeta();
-            bookMeta.setTitle("Go To");
-            bookMeta.addPage("First line: 1\nCurrent line: "+currentLine+"\nFinial line: "+max+"\n\nGo To: ");
-            book.setItemMeta(bookMeta);
-            player.openBook(book);
+            int currentLine = e.getCurrentItem().getItemMeta().getPersistentDataContainer().get(new NamespacedKey(JavaPlugin.getPlugin(FileManager.class), "currentLine"), PersistentDataType.INTEGER);
+
+            ItemStack paper = itemProperties(new ItemStack(Material.PAPER), String.valueOf(currentLine), null);
+            ItemMeta meta = paper.getItemMeta();
+            meta.getPersistentDataContainer().set(new NamespacedKey(JavaPlugin.getPlugin(FileManager.class), "max"), PersistentDataType.INTEGER, max);
+            meta.getPersistentDataContainer().set(new NamespacedKey(JavaPlugin.getPlugin(FileManager.class), "type"), PersistentDataType.STRING, "goto");
+            paper.setItemMeta(meta);
+
+            new AnvilGUI.Builder()
+                    .plugin(JavaPlugin.getPlugin(FileManager.class))
+                    .preventClose()
+                    .title("Min: 1, Max: "+max)
+                    .itemLeft(paper)
+                    .onClick((slot, stateSnapshot) -> {
+                        if (slot == AnvilGUI.Slot.OUTPUT) {
+                            return List.of(AnvilGUI.ResponseAction.close());
+                        }
+                        return Collections.emptyList();
+                    })
+                    .onClose(stateSnapshot -> {
+                        int line;
+                        if (stateSnapshot.getOutputItem().getItemMeta() != null) {
+                            try {
+                                line = Integer.parseInt(stateSnapshot.getOutputItem().getItemMeta().getDisplayName().trim());
+                            } catch (NumberFormatException nfe) {
+                                line = 1;
+                            }
+                        }
+                        else line = 1;
+                        openFile(stateSnapshot.getPlayer(), line);
+                    })
+                    .open(player);
         }
     }
 
@@ -237,38 +301,43 @@ public class FileGui implements Listener {
     public void edit(InventoryClickEvent e) {
         if (e.getWhoClicked() instanceof Player player) {
             if (e.getCurrentItem() == null || e.getCurrentItem().getItemMeta() == null || e.getCurrentItem().getType() != Material.PAPER || !position.containsKey(player.getName())) return;
-            int line = e.getCurrentItem().getItemMeta().getPersistentDataContainer().get(new NamespacedKey(JavaPlugin.getPlugin(FileManager.class), "line"), PersistentDataType.INTEGER);
-            int offset = e.getCurrentItem().getItemMeta().getPersistentDataContainer().get(new NamespacedKey(JavaPlugin.getPlugin(FileManager.class), "offset"), PersistentDataType.INTEGER);
-            PathHolder pathHolder = position.get(player.getName());
+            if (checkType(e.getCurrentItem(), "text")) return;
+            if (Boolean.TRUE.equals(e.getCurrentItem().getItemMeta().getPersistentDataContainer().get(new NamespacedKey(JavaPlugin.getPlugin(FileManager.class), "edited"), PersistentDataType.BOOLEAN))) return;
 
-            player.closeInventory();
-            player.openInventory(Bukkit.createInventory(null, InventoryType.ANVIL));
+            ItemStack paper = e.getCurrentItem();
+            ItemMeta meta = paper.getItemMeta();
+            meta.getPersistentDataContainer().set(new NamespacedKey(JavaPlugin.getPlugin(FileManager.class), "edited"), PersistentDataType.BOOLEAN, true);
+            meta.getPersistentDataContainer().set(new NamespacedKey(JavaPlugin.getPlugin(FileManager.class), "type"), PersistentDataType.STRING, "edit");
+            paper.setItemMeta(meta);
 
-
-            try {
-                BufferedReader br = new BufferedReader(new FileReader(pathHolder.getCurrentPath()));
-                BufferedWriter bw = new BufferedWriter(new FileWriter(pathHolder.getCurrentPath()+"temp"));
-
-                String text;
-                int i = 0;
-                while ((text = br.readLine()) != null) {
-                    if (line == i) {
-                        String start = text.substring(0, 35*offset);
-                        String replace = text.substring(35*offset, 35*offset+1);
-                        String end = text.substring(35*offset+1);
-                        System.out.println(start);
-                        System.out.println(replace);
-                        System.out.println(end);
-                    }
-                    bw.write(text);
-                    i++;
-                }
-
-            } catch (Exception ex) {
-                player.sendMessage(ChatColor.RED+"There was an error trying to edit that file.\nPlease see the console for error message and report this.");
-                ex.printStackTrace();
-                return;
-            }
+            new AnvilGUI.Builder()
+                    .plugin(JavaPlugin.getPlugin(FileManager.class))
+                    .preventClose()
+                    .title("File editor:")
+                    .itemLeft(e.getCurrentItem())
+                    .itemOutput(paper)
+                    .onClick((slot, stateSnapshot) -> {
+                        if (slot == AnvilGUI.Slot.OUTPUT) {
+                            return List.of(AnvilGUI.ResponseAction.close());
+                        }
+                        return Collections.emptyList();
+                    })
+                    .onClose(stateSnapshot -> {
+                        editFile(stateSnapshot.getPlayer(), stateSnapshot.getOutputItem());
+                        Integer line;
+                        if (stateSnapshot.getOutputItem().getItemMeta() != null)
+                            line = stateSnapshot.getOutputItem().getItemMeta().getPersistentDataContainer().get(new NamespacedKey(JavaPlugin.getPlugin(FileManager.class), "line"), PersistentDataType.INTEGER);
+                        else  line = 1;
+                        if (line == null || line < 1) line = 1;
+                        openFile(stateSnapshot.getPlayer(), line);
+                    })
+                    .open(player);
         }
+    }
+
+    public static boolean checkType(ItemStack item, String type) {
+        ItemMeta meta = item.getItemMeta();
+        if (meta == null) return true;
+        return !type.equals(meta.getPersistentDataContainer().get(new NamespacedKey(JavaPlugin.getPlugin(FileManager.class), "type"), PersistentDataType.STRING));
     }
 }
